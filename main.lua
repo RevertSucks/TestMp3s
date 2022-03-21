8ccc2834a91616c3acaa918689135fdc58c657a7
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2022-03-21 11:06:46 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ for i,v in pairs(response) do
     if type(v) == "table" then
         for i2,v2 in pairs(v) do
             if string.find(v2,"filename") then
-                _G.filename = v2:gsub("^(.-)=","")
+                _G.filename = v2:gsub("^(.-)/","")
             end
         end
     end
